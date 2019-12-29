1ce4117cee8cbd34742a99660c02aeae8ec80d9f
James <triskept@gmail.com>
2019-12-29 12:20:02 -0500

    Initial commit, setup started

---
+++ UniHospital/Database.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniHospital
+{
+    class Database
+    {
+        public bool DatabaseOff { get; set; }
+
+        public Database()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("\nWelcome to University Hospital's Database!");
+            Console.WriteLine("\nPress enter to explore info..\n");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("------------------------------");
+
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        public void BootUp()
+        {
+
+        }
+    }
+}
---
+++ UniHospital/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UniHospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool databaseOn = true;
+            while (databaseOn)
+            {
+                Database db = new Database();
+
+                while (db.DatabaseOff == false)
+                {
+                    db.BootUp();
+                }
+                databaseOn = false;
+            }
+        }
+    }
+}
